b707108901757861c6222575eece692d3a1738b5
PiotrJZielinski <piotrekzie100@gmail.com>
2019-03-03 00:02:17 +0100

    Add initial structure of the project

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyTransdec",
+    version="0.0.1",
+    author="Piotr Zielinski",
+    author_email="piotr_zielinski@g.pl",
+    description="Transdec environment https://github.com/PiotrJZielinski/TransdecEnvironment controlling with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PiotrJZielinski/PyTransdec",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
