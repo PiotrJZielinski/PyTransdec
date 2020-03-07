ce4f9d4eb2064199341e51eb0afb8dbb584af817
Szymon Korczynski <Szymonkor8@gmail.com>
2020-03-07 20:38:55 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyTransdec",
-    version="0.1.0",
+    version="0.2.0",
     author="Piotr Zielinski",
     author_email="piotr_zielinski@g.pl",
-    description="Transdec environment https://github.com/PiotrJZielinski/TransdecEnvironment controlling with Python",
+    description="Transdec environment https://github.com/pwrdc/TransdecEnvironment controlling with Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/PiotrJZielinski/PyTransdec",
+    url="https://github.com/pwrdc/PyTransdec",
     packages=setuptools.find_packages(),
     install_requires=[
         'numpy',
         'pandas',
         'tqdm',
         'opencv-python',
-        'mlagents'
+        'mlagents==0.7.0'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
